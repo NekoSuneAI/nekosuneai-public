5141985e7bbe470ca0ce77db97bc1ce0b01b4f3d
NekoSuneVR <chisdeal2014@hotmail.com>
2025-08-27 09:54:42 +0100

    aaa

--- Modules/AUTOAI/VOICEModules/Main.js
+++ Modules/AUTOAI/VOICEModules/Main.js
@@ -38,7 +38,7 @@ function startRecordingAndRunDeepSpeech() {
   const recorder = record.record({
     sampleRate: 16000,
     endOnSilence: true,
-    recorder: "sox"
+    recorder: "arecord"
   });
   const fileStream = fs.createWriteStream(audioFile, {
     encoding: "binary"
